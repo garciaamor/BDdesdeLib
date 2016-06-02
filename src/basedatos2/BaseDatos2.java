a217ccb4915d15ba71ef99ca3064d8295075f047
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2016-06-02 09:01:34 +0200

    Prueba Datos con Libreria añadida

---
+++ src/basedatos2/BaseDatos2.java
@@ -0,0 +1,29 @@
+package basedatos2;
+
+public class BaseDatos2 {
+
+    public static void main(String[] args) {
+        mets.metodos met = new mets.metodos();
+        
+      
+          
+      
+      
+     
+     met.Conectar("jdbc:mysql://10.0.0.254/jgarciaamor","jgarciaamor","jgarciaamor");
+           
+        
+     
+     
+    // met.insertar("Contornos","ID, Titulo", "2,'segundo titulo'");
+     
+     //met.insertar("Contornos","ID, Titulo", "3,'tercer titulo'");
+        
+     //met.consultaDatos("Contornos", 2, "ID, Titulo");
+        
+     //met.borrar("Contornos",3);
+        
+     //met.actualizar("Contornos","1","Titulo","nuevo primer titulo");
+    }
+    
+}
